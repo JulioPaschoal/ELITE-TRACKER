3fa721b40b7acce06fb6550175543fe072cdd047
JulioPaschoal <juliocpaschoal@gmail.com>
2025-11-06 18:54:56 -0300

    tratando erros externos e definindo variaveis de ambiente

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,8 +1,8 @@
-import axios from 'axios';
+import axios, { isAxiosError } from 'axios';
 import { Request, Response } from 'express';
 
-const clientId = 'Ov23liBC3WtQuPqkolrC';
-const clientSecret = '142209e2aaf07a43066b3edcbb97fc658592fd3b';
+const { GITHUB_CLIENT_ID: clientId, GITHUB_CLIENT_SECRET: clientSecret } =
+  process.env;
 
 //Redirecionando para o GitHub \\
 export class AuthController {
@@ -13,30 +13,37 @@ export class AuthController {
 
   // Autenticação com o GitHub \\
   authCallback = async (req: Request, res: Response) => {
-    const { code } = req.query;
-    const accessTokenResult = await axios.post(
-      'https://github.com/login/oauth/access_token',
-      {
-        client_id: clientId,
-        client_secret: clientSecret,
-        code,
-      },
-      {
-        headers: {
-          Accept: 'application/json',
+    try {
+      const { code } = req.query;
+      const accessTokenResult = await axios.post(
+        'https://github.com/login/oauth/access_token',
+        {
+          client_id: clientId,
+          client_secret: clientSecret,
+          code,
         },
-      },
-    );
-    const userDataResult = await axios.get('https://api.github.com/user', {
-      headers: {
-        Authorization: `Bearer ${accessTokenResult.data.access_token}`,
-      },
-    });
-    const {
-      node_id: id,
-      avatar_url: avatarUrl,
-      login: username,
-    } = userDataResult.data;
-    return res.status(200).json({ id, avatarUrl, username });
+        {
+          headers: {
+            Accept: 'application/json',
+          },
+        },
+      );
+      const userDataResult = await axios.get('https://api.github.com/user', {
+        headers: {
+          Authorization: `Bearer ${accessTokenResult.data.access_token}`,
+        },
+      });
+      const {
+        node_id: id,
+        avatar_url: avatarUrl,
+        login: username,
+      } = userDataResult.data;
+      return res.status(200).json({ id, avatarUrl, username });
+    } catch (err) {
+      if (isAxiosError(err)) {
+        return res.status(400).json(err.response?.data);
+      }
+      return res.status(500).json({ message: 'Something went wrong' });
+    }
   };
 }
