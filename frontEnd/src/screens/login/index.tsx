3d6a5bfbfafa3c1b074be2b6ec4ef720bac426fa
JulioPaschoal <juliocpaschoal@gmail.com>
2025-11-25 20:25:01 -0300

    criando componente de button

--- frontEnd/src/screens/login/index.tsx
+++ frontEnd/src/screens/login/index.tsx
@@ -1,15 +1,15 @@
 import { GithubLogo } from '@phosphor-icons/react';
 
+import { Button } from '../../components/button';
 import styles from './styles.module.css';
 export function Login() {
   return (
     <div className={styles.container}>
       <div className={styles.content}>
         <h1>Entrar com</h1>
-        <button>
-          {' '}
+        <Button>
           <GithubLogo /> GitHub
-        </button>
+        </Button>
         <p>
           Ao entrar, eu concordo com o{' '}
           <span>Termos de Serviço e Política de Privacidade</span>
